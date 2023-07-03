8b2f2086f7ab0ca34c4d40176fc27961fd4f667f
Md. Zayed Oyshik <zayedoyshik1@gmail.com>
2023-07-03 18:46:40 +0600

    Scrollable HotelScreen

--- lib/screens/hotel_screen.dart
+++ lib/screens/hotel_screen.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 import 'package:traver/utils/app_layout.dart';
 import 'package:traver/utils/app_styles.dart';
@@ -9,20 +11,53 @@ class HotelScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     final size = AppLayout.getSize(context);
     return Container(
-      padding: EdgeInsets.only(right: 16),
+      padding: EdgeInsets.all(16),
+      margin: EdgeInsets.only(right: 16),
       width: size.width*0.6,
       height: 350,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(20),
+        gradient: LinearGradient(
+          begin: Alignment.topCenter,
+          end: Alignment.bottomCenter,
+          colors: [
+            Color.fromARGB(255, 233, 233, 233),
+            Color.fromARGB(255, 192, 192, 192),
+          ],
+        )
+      ),
       child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          Container(
-            height: 180,
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(20),
-              color: Styles.primaryColor,
-              image: DecorationImage(image: AssetImage("assets/images/one.png"), fit: BoxFit.cover),
-            ),
-          ),
+          HotelImage(hotelImage: "assets/images/one.png",),
+          SizedBox(height: 10,),
+          Text("Luxurious", style: Styles.headLineStyle2.copyWith(fontWeight: FontWeight.w900)),
+          SizedBox(height: 10,),
+          Text("London", style: Styles.headLineStyle3.copyWith(fontWeight: FontWeight.w900)),
+          SizedBox(height: 10,),
+          Text("\$30/night", style: Styles.headLineStyle1.copyWith(fontWeight: FontWeight.w900)),
+          SizedBox(height: 10,),
         ],
+      ),
+    );
+  }
+}
+
+class HotelImage extends StatelessWidget {
+  final String hotelImage;
+
+  const HotelImage({super.key, required this.hotelImage});
+  
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 180,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(20),
+        // color: Styles.primaryColor,
+        image: DecorationImage(image: AssetImage(hotelImage), fit: BoxFit.cover),
+        
       ),
     );
   }
